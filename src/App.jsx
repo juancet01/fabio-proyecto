d45efedc3456098e65e0b9d8fabebbc00c1d2db0
juanceto01 <matusauriorex@gmail.com>
2026-08-11 21:48:50 -0300

    11/8

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+function App() {
+  return (
+    <div style={{ padding: '10px'}}>
+      <h1>titulo</h1>
+    <nav className="vertical-nav"></nav>
+      <nav style={{ background: '#a8ff08', padding: '10px', borderRadius: '5px' }}>
+        <a href="nicio" style={{ color: 'white', marginRight: '15px' }}>Inicio</a>
+        <a href="productos" style={{ color: 'white', marginRight: '15px' }}>Productos</a>
+        <a href="contacto" style={{ color: 'white' }}>Contacto</a>
+      </nav>
+
+      <div
+      ></div>
+    </div>
+
+    
+  )
+}
+export default App
